9781b049341bf5650c902c58b3751d559a0cd005
Andreas Herkert <a.herkert@kabelbw.de>
2023-05-13 17:18:36 +0200

    andis blog blog blog

--- src/partials/Navbar.tsx
+++ src/partials/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = () => (
               <path d="M4 20h14"></path>
             </svg>
           }
-          name="Andi's Blog"
+          name="Andi's Blog Blog Blog"
         />
       </a>
 
