b8f2cfecab970f2bb7ee8eaf519ed918d007ade8
Andreas Herkert <a.herkert@kabelbw.de>
2023-09-09 14:57:17 +0200

    Änderung Webmaster

--- src/partials/Navbar.tsx
+++ src/partials/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = () => (
               <path d="M4 20h14"></path>
             </svg>
           }
-          name="Andi's Blog Blog Blog Blog"
+          name="Designmaster"
         />
       </a>
 
